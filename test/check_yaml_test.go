a2c52dcb0386ebceb48490b0d86147cc891281d1
Hugo Barros <hugo.barros@caylent.com>
2021-07-16 11:02:39 -0300

    CA-23 adding initial tests: yaml check

---
+++ test/check_yaml_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+)
+
+func TestYamlCheck(t *testing.T) {
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../test_examples/test_minimal",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	rendered := terraform.Output(t, terraformOptions, "tamr-config")
+	require.NotNil(t, rendered)
+
+	// allocates a new map so that we can pass its address to the `yaml.Unmarshal` function
+	m := make(map[interface{}]interface{})
+
+	// converts the string rendered to a list of bytes and assigns decoded values to `m` as a
+	// map that we may type cast later
+	err := yaml.Unmarshal([]byte(rendered), &m)
+	require.NoError(t, err)
+
+	fmt.Printf("--- m:\n%v\n\n", m)
+}
