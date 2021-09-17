cfc5ed4e33d4ec4b96bde1fa8a60f43ec988d2ed
Hugo Barros <hugo.barros@caylent.com>
2021-09-17 10:59:20 -0300

    adding t.Run() to have more separated test functions

--- test/check_yaml_test.go
+++ test/check_yaml_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v3"
 )
@@ -11,7 +12,7 @@ import (
 func TestYamlCheck(t *testing.T) {
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../test_examples/test_minimal",
+		TerraformDir: "../test_examples/minimal",
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
@@ -19,13 +20,18 @@ func TestYamlCheck(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	rendered := terraform.Output(t, terraformOptions, "tamr-config")
-	require.NotNil(t, rendered)
 
-	// allocates a new map so that we can pass its address to the `yaml.Unmarshal` function
-	m := make(map[interface{}]interface{})
+	t.Run("validate_output_exists", func(t *testing.T) {
+		require.NotNil(t, rendered)
+	})
 
-	// converts the string rendered to a list of bytes and assigns decoded values to `m` as a
-	// map that we may type cast later
-	err := yaml.Unmarshal([]byte(rendered), &m)
-	require.NoError(t, err)
+	t.Run("validate_unmarshall_to_yaml", func(t *testing.T) {
+		// allocates a new map so that we can pass its address to the `yaml.Unmarshal` function
+		m := make(map[interface{}]interface{})
+
+		// converts the string rendered to a list of bytes and assigns decoded values to `m` as a
+		// map that we may type cast later
+		err := yaml.Unmarshal([]byte(rendered), &m)
+		assert.NoError(t, err)
+	})
 }
