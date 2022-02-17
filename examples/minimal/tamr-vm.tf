c5ab652ff45612d03280683813b45a7b8d4a549d
Federico Vazquez <federico.vazquez@caylent.com>
2022-02-17 17:24:39 -0300

    Tests without product-code

--- examples/minimal/tamr-vm.tf
+++ examples/minimal/tamr-vm.tf
@@ -4,18 +4,11 @@ locals {
 
 data "aws_ami" "tamr-vm" {
   most_recent = true
-  #owners      = ["679593333241"]
-  owners      = ["amazon"]
-  #name_regex  = "^Ubuntu 18.04 Tamr.*"
-  filter {
-   # name   = "product-code"
-    #values = ["832nkbrayw00cnivlh6nbbi6p"]
-    name = "name"
-    values = ["amzn-ami-hvm-*-x86_64-gp2"]
-  }
+  owners      = ["679593333241"]
+  name_regex  = "^Ubuntu 18.04 Tamr.*"
   # filter {
-  #   name   = "product-code.type"
-  #   values = ["marketplace"]
+  #   name   = "product-code"
+  #   values = ["832nkbrayw00cnivlh6nbbi6p"]
   # }
 }
 
